224599ca52d59209bc61bde3e55ffa44961501d5
kuga yoshihiko <horrormovies.noga@gmail.com>
2021-12-06 12:29:58 +0900

    feat: styled-components 使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+
 import logo from "./logo.svg";
 import "./App.css";
+
+const StyledTest = styled.div`
+  background-color: blue;
+  color: red;
+`;
+
+// `ts-ignore` でエラーを無視しないといけない記述
+// const ReStyledTest = styled(StyledTest)`
+//   background-color: green;
+// `;
+
+const ReStyledTest = styled<
+  // StyledComponent でない型の扱いにすることで `Type of property 'defaultProps' ～` のエラーを回避
+  React.FC<typeof StyledTest["defaultProps"] & { test?: boolean }>
+>(
+  ({
+    // StyledTest には不要となる変数を除去
+    test,
+    ...props
+  }) => <StyledTest {...props} />
+)`
+  background-color: green;
+  color: ${(props) => (props.test ? "black" : "white")};
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <StyledTest>StyledTest</StyledTest>
+      <ReStyledTest>ReStyledTest</ReStyledTest>
     </div>
   );
 }
